098592002ff6bacd7b2248fb61c90a0d438a43f6
sourav-gls <souravghosh.gls@gmail.com>
2026-03-08 01:06:27 +0600

    Load and display data in other two button

--- home.js
+++ home.js
@@ -1,7 +1,18 @@
 const cardContainer = document.getElementById("card-container");
+const totalCount = document.getElementById("total-count");
+const openBtn = document.getElementById("open-btn");
+const closedBtn = document.getElementById("closed-btn");
+const allBtn = document.getElementById("all-btn");
+
 
 
 function loadAllCard(){
+   allBtn.classList.add("btn-primary") ;
+   closedBtn.classList.remove("btn-primary");
+   openBtn.classList.remove("btn-primary");
+   
+   
+
     fetch(" https://phi-lab-server.vercel.app/api/v1/lab/issues ")
     .then(res=>res.json())
     .then(data=>{
@@ -9,32 +20,100 @@ function loadAllCard(){
     })
 }
 
+loadAllCard()
+
+
+function loadOpenCard(){
+   let cards = [];
+   openBtn.classList.add("btn-primary");
+   closedBtn.classList.remove("btn-primary");
+   allBtn.classList.remove("btn-primary") ;
+   
+   fetch(" https://phi-lab-server.vercel.app/api/v1/lab/issues ")
+    .then(res=>res.json())
+    .then(data=>{
+       cards = data.data;
+       console.log(cards);
+       const openCard = cards.filter(card=> card.status ==="open")
+       displayCard(openCard)
+    })
+
+}
+
+function loadClosedCard(){
+   let cards = [];
+   closedBtn.classList.add("btn-primary");
+   openBtn.classList.remove("btn-primary");
+   allBtn.classList.remove("btn-primary") ;
+   
+   fetch(" https://phi-lab-server.vercel.app/api/v1/lab/issues ")
+    .then(res=>res.json())
+    .then(data=>{
+       cards = data.data;
+       console.log(cards);
+       const closedCard = cards.filter(card=> card.status ==="closed")
+       displayCard(closedCard)
+    })
+
+}
+
+
+
 function displayCard(items){
     cardContainer.innerHTML = "" ;
    for (let item of items){
-    console.log(item);
     const div = document.createElement("div");
-    div.className = "bg-white rounded-md border border-red-500 p-4 shadow-md";
+    div.className = "bg-white rounded-md  p-4 shadow-md ";
+    div.classList.add()
     div.innerHTML = `
-    <div class="flex justify-between">
-                <img src="./assets - Copy/Open-Status.png";alt="">
-                <div class="badge badge-soft badge-secondary">High</div>
+    <div class="flex justify-between ">
+                <div>
+                <img src="${item.status === 'open'? './assets - Copy/Open-Status.png' : './assets - Copy/Closed- Status .png'}";alt="">
+                </div>
+                <div>${priority(item.priority)}</div>
              </div>
-             <div class="pt-3 space-y- ">
-                <h5 class="font-bold text-[14px] line-clamp-2">${item.title}</h5>
+             <div class="pt-3 space-y-2 ">
+                <h5 class="font-bold text-[16px] min-h-[42px] ">${item.title}</h5>
                 <p class="text-[#64748B] text-[12px] line-clamp-2">${item.description}</p>
-                <div class="flex gap-1 mt-2">
-                     <div class="badge badge-soft badge-secondary">High</div>
-                     <div class="badge badge-soft badge-secondary">High</div>
+                <div class=" space-x-1 mt-2">
+                     ${levels(item.labels)}
                 </div>
              </div>
              <hr class="border-gray-200 mt-4 mb-4">
              <div class="text-[#64748B] space-y-2 text-[12px]">
                 <p>#${item.id} by ${item.author}</p>
-                <p>1/15/2024</p>
+                <p>${item.createdAt}</p>
                       
              </div>  `
     cardContainer.append(div);
    }
+   totalCount.innerText = cardContainer.children.length;
 }
-loadAllCard()
+
+
+
+
+
+
+function priority(id){
+   const priorityBtn = document.createElement("div")
+   if(id==="high"){
+      return `<span class ="badge text-red-500 bg-red-100 rounded-2xl  text-[12px]">HIGH<span>`
+   }
+   else if(id==="medium"){
+      return `<span class ="badge text-yellow-500 bg-yellow-100 rounded-2xl text-[12px]">MEDIUM<span>`
+   }
+   else{
+      return `<span class ="badge bg-gray-100 text-[#64748B] rounded-2xl text-[12px]">LOW<span>`
+   }
+}
+
+
+
+function levels(arr){
+   let highlight = "";
+for(let i=0 ; i<arr.length ; i++){
+   highlight += `<span class ="badge bg-lime-100">${arr[i]}</span>`;
+}
+   return highlight ;
+}
